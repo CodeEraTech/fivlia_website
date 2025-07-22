4e3af2978315d8708f26baa89e63ff329b6c892b
Vivek Gupta <vivekhappy2002@gmail.com>
2025-07-22 08:49:29 +0530

    home page banner, category, popular products, featured products, product details changes done

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -312,10 +312,10 @@ const Footer = () => {
                   © {year} All Rights Reserved By _
                   
                   <Link
-                    to="https://nigammishra.github.io/nigam-portfolio/"
+                    to="#"
                     target="_blank"
                   >
-                     @nigammishra
+                     @fivlia
                   </Link>
                 </div>
               </div>
