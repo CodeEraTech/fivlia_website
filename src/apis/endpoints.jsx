aa734dcaa18ac1e5dc7190a701c6b9d384020897
Vivek <codeeradevelopers@gmail.com>
2025-10-30 15:45:57 +0530

    changes

--- src/apis/endpoints.jsx
+++ src/apis/endpoints.jsx
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "https://api.fivlia.com";
+export const API_BASE_URL = "https://api.fivlia.in";
 // export const API_BASE_URL = "http://localhost:8080";
 // export const API_BASE_URL = "http://127.0.0.1:8080";
 
